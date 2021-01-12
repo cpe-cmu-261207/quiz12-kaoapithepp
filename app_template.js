c69c44562e724bb3682bfaa710253a54fa57425d
kaoapithepp <simsonslt.ig@gmail.com>
2021-01-12 13:52:40 +0700

    add-credit

--- app_template.js
+++ app_template.js
@@ -1,3 +1,4 @@
+//Quiz 1.1
 let inputNumber = document.getElementById('input-number');
 let message = document.getElementById('compute-result')
 let buttonCom = document.getElementById('button-compute')
@@ -9,3 +10,17 @@ function plusID() {
 }
 
 buttonCom.addEventListener('click', plusID)
+
+
+//Quiz 1.2
+let inputRow = document.getElementById('input-row')
+let inputCol = document.getElementById('input-col')
+let genButton = document.getElementById('button-create-table')
+//let showCredit = document.getElementById('show-credit')
+
+
+function tableGen() {
+    
+}
+
+genButton.addEventListener('click', tableGen)
