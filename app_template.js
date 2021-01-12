8015d46ccaf790fab2c822c4fcaa614e89414049
kaoapithepp <simsonslt.ig@gmail.com>
2021-01-12 13:42:42 +0700

    first-commit

--- app_template.js
+++ app_template.js
@@ -1,1 +1,11 @@
-//your code here!
+let inputNumber = document.getElementById('input-number');
+let message = document.getElementById('compute-result')
+let buttonCom = document.getElementById('button-compute')
+
+function plusID() {
+    let number = Number(inputNumber.value)
+    let outputHtml = String(620612168+number)
+    message.innerHTML = outputHtml
+}
+
+buttonCom.addEventListener('click', plusID)
